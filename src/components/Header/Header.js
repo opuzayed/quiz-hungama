e709d16fecfeaa5cec96b1560a7cef964732d35c
opuzayed <zayedopu4933@gmail.com>
2022-10-11 14:03:25 +0600

    navbar done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
 const Header = () => {
-    return (
-        <div>
-           <div>
-            <h1>Quiz Hungama</h1>
-            </div> 
-            <div>
-                <Link to='/'>Home</Link>
-                <Link to='/topics'>Topics</Link>
-                <Link to='/statistics'>Statistics</Link>
-                <Link to='/blog'>Blog</Link>
-
-            </div>
-        </div>
-    );
+  return (
+   <div className="header">
+      <div>
+        <h1>Quiz Hungama</h1>
+      </div>
+      <div className="header-link">
+        <Link to="/">Home</Link>
+        <Link to="/topics">Topics</Link>
+        <Link to="/statistics">Statistics</Link>
+        <Link to="/blog">Blog</Link>
+      </div>
+    </div> 
+    
+  );
 };
 
 export default Header;
