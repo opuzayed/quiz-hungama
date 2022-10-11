19be62dce429f9a54bb27a2ef45d8332b6eda664
opuzayed <zayedopu4933@gmail.com>
2022-10-11 15:46:54 +0600

    header part done

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
         children:[
           {
             path:'/',
+            
             element:<Home></Home>
           },
           {
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,16 @@
 import React from 'react';
-
+import './Home.css';
 const Home = () => {
+
     return (
-        <div>
-            
-        </div>
+         <div className='container'>
+            {/* <img src='https://image.shutterstock.com/image-vector/vector-realistic-isolated-neon-sign-260nw-1343046227.jpg' alt='QuizPIC'></img> */}
+            <img src='https://previews.123rf.com/images/peshkov/peshkov1911/peshkov191100886/134138737-success-and-knowledge-concept-creative-business-sketch-on-chalkboard-blackboard-wall-background-3d-r.jpg' alt='QuizPIC'></img> 
+           <div className='content'>
+            <p>This site is test your skill on different types of tech languages,such as reactjs, javaScript,CSS, git.Our main motive to test your knowledge specific on topics.</p> 
+           </div>
+        </div> 
+        
     );
 };
 
