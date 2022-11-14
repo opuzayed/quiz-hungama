736c179de9a92af2284f6061d83600b867b96fb6
opuzayed <zayedopu4933@gmail.com>
2022-11-15 02:06:46 +0600

    toast show on ui

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,14 +1,18 @@
 import React from "react";
+import toast, { Toaster } from "react-hot-toast";
 
 const Quiz = ({ q }) => {
-    
   console.log(q);
   const { question, options } = q;
-  function onChangeValue(event) {
-    // setGender(event.target.value);
-    
-    console.log(event.target);
-  }
+  const onChangeValue = (event) => {
+    const targetValue = event.target.value;
+    const val = q.correctAnswer;
+    if (targetValue === val) {
+      toast.success("Correct Answer");
+    } else {
+      toast.success("Wrong Answer");
+    }
+  };
   return (
     <div>
       <div>
@@ -19,6 +23,7 @@ const Quiz = ({ q }) => {
           <input
             className="form-check-input"
             type="radio"
+            value={options[0]}
             name="flexRadioDefault"
             id="flexRadioDefault1"
           />
@@ -32,11 +37,12 @@ const Quiz = ({ q }) => {
           <input
             className="form-check-input"
             type="radio"
+            value={options[1]}
             name="flexRadioDefault"
             id="flexRadioDefault1"
           />
           <label className="form-check-label" HTMLFor="flexRadioDefault1">
-          {options[1]}
+            {options[1]}
           </label>
         </div>
       </div>
@@ -45,11 +51,12 @@ const Quiz = ({ q }) => {
           <input
             className="form-check-input"
             type="radio"
+            value={options[2]}
             name="flexRadioDefault"
             id="flexRadioDefault1"
           />
           <label className="form-check-label" HTMLFor="flexRadioDefault1">
-          {options[2]}
+            {options[2]}
           </label>
         </div>
       </div>
@@ -58,14 +65,16 @@ const Quiz = ({ q }) => {
           <input
             className="form-check-input"
             type="radio"
+            value={options[3]}
             name="flexRadioDefault"
             id="flexRadioDefault1"
           />
           <label className="form-check-label" HTMLFor="flexRadioDefault1">
-          {options[3]}
+            {options[3]}
           </label>
         </div>
       </div>
+      <Toaster />
     </div>
   );
 };
