03192e902f9dc8f9876001f87586d25537a41ba8
opuzayed <zayedopu4933@gmail.com>
2022-11-15 15:05:03 +0600

    blog done

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,9 +1,23 @@
 import React from 'react';
-
+import './Blog.css';
 const Blog = () => {
     return (
-        <div>
-            <p>blog</p>
+        <div className='blog-container'>
+            <div>
+                <h3>What is the purpose of react router?</h3>
+                <p>Routing is a process in which a user is directed to different pages based on their action or request. ReactJS Router is mainly used for developing Single Page Web Applications. React Router is used to define multiple routes in the application. When a user types a specific URL into the browser, and if this URL path matches any 'route' inside the router file, the user will be redirected to that particular route.</p>
+            </div>
+            <div>
+                <h3>How does context api works?</h3>
+                <p>The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.</p>
+            </div>
+            <div>
+                <h3>Brief introduction about useRef?</h3>
+                <p>
+                Ref means just reference, so it can be a reference to anything (DOM node, Javascript value, etc). The useRef hook returns a mutable object which doesn’t re-render the component when it’s changed. Think it like useState, but unlike useState, doesn’t trigger re-render of the component. The object that useRef returns have a current property that can hold any modifiable value. 
+                </p>
+            </div>
+           
         </div>
     );
 };
